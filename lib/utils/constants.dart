fdda9eb8d1a60833bf01b5c050710eab491dab8b
Sarah Tariq <sarahtariq971@gmail.com>
2026-01-08 11:56:54 +0500

    Add utility helper functions

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String appName = 'Currency Converter';
+  static const String defaultFromCurrency = 'USD';
+  static const String defaultToCurrency = 'EUR';
+  static const double defaultAmount = 1.0;
+}
